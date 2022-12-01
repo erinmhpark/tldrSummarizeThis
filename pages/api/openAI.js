1af056a3916360f9f0e57ae309c2aaa52ea15065
Erin Park <b0ldrobott@Erins-MBP.attlocal.net>
2022-12-01 00:36:17 -0800

    Remove comments

--- pages/api/openAI.js
+++ pages/api/openAI.js
@@ -1,24 +1,3 @@
-// import { Configuration, OpenAIApi } from "openai";
-// const configuration = new Configuration({
-//   apiKey: process.env.OPENAI_API_KEY,
-// });
-
-// const openai = new OpenAIApi(configuration);
-
-// export default async function handler(req, res) {
-//   const completion = await openai.createCompletion({
-//     model: "text-davinci-003",
-//     prompt:
-//       "A neutron star is the collapsed core of a massive supergiant star, which had a total mass of between 10 and 25 solar masses, possibly more if the star was especially metal-rich.[1] Neutron stars are the smallest and densest stellar objects, excluding black holes and hypothetical white holes, quark stars, and strange stars.[2] Neutron stars have a radius on the order of 10 kilometres (6.2 mi) and a mass of about 1.4 solar masses.[3] They result from the supernova explosion of a massive star, combined with gravitational collapse, that compresses the core past white dwarf star density to that of atomic nuclei.\n\nTl;dr",
-//     temperature: 0.7,
-//     max_tokens: 60,
-//     top_p: 1.0,
-//     frequency_penalty: 0.0,
-//     presence_penalty: 1,
-//   });
-//   res.status(200).json({ result: completion.data });
-// }
-
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
