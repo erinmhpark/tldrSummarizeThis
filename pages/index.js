0cb6c765380f46641c9ade8b91690a67fdbc63c5
Erin Park <b0ldrobott@Erins-MBP.attlocal.net>
2022-11-30 23:55:44 -0800

    Build submit button

--- pages/index.js
+++ pages/index.js
@@ -13,9 +13,8 @@ export default function Home() {
 
     const formData = new FormData(event.target);
     const data = Object.fromEntries(formData);
-    const initialPrompt =
-      "Summarize this for a second-grade student:\n" + data.prompt;
-    console.log(initialPrompt);
+    const initialPrompt = "Summarize this:\n" + data.prompt;
+    // console.log(initialPrompt);
     // console.log(formData);
     // console.log(data.prompt);
 
@@ -52,55 +51,55 @@ export default function Home() {
             name="prompt"
             disabled={loading}
           />
-          <div className="flex justify-evenly">
+          <div className="flex justify-evenly py-5">
             <input type="reset" />
-            <input
+            <button
+              type="Submit"
+              className={cn(
+                "inline-flex items-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md text-white bg-indigo-500 hover:bg-indigo-400 transition ease-in-out duration-150",
+                loading ? "cursor-not-allowed" : ""
+              )}
+              disabled={loading}
+            >
+              {loading ? (
+                <svg
+                  className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    strokeWidth="4"
+                  ></circle>
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                  ></path>
+                </svg>
+              ) : (
+                "Submit"
+              )}
+            </button>
+            {/* <input
               className="border border-white p-1 rounded-md disabled:bg-red-500"
               type="submit"
               disabled={loading}
-            />
+            /> */}
           </div>
-          <button
-            type="Submit"
-            className={cn(
-              "inline-flex items-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md text-white bg-indigo-500 hover:bg-indigo-400 transition ease-in-out duration-150",
-              loading ? "cursor-not-allowed" : ""
-            )}
-            disabled={loading}
-          >
-            {loading ? (
-              <svg
-                className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <circle
-                  className="opacity-25"
-                  cx="12"
-                  cy="12"
-                  r="10"
-                  stroke="currentColor"
-                  strokeWidth="4"
-                ></circle>
-                <path
-                  className="opacity-75"
-                  fill="currentColor"
-                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                ></path>
-              </svg>
-            ) : (
-              "Submit"
-            )}
-          </button>
         </form>
 
-        <div className="flex justify-between border">
-          <div className="p-1">Original:&nbsp; </div>
-          <div className="p-1">Summary:</div>
+        <div className="flex flex-col justify-between w-1/2 ">
+          <div className="p-1 border">Original:&nbsp; </div>
+          <div className="p-1 border">
+            Summary: {summary ? <p className="w-100">{summary}</p> : null}
+          </div>
         </div>
-
-        {summary ? <p>{summary}</p> : null}
       </main>
 
       <footer className="flex justify-evenly p-2 static bottom-0">
