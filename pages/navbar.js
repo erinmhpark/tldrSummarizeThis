c7cc9ec51cb5b83988bd8c9611788f071b119c33
Erin Park <b0ldrobott@Erins-MBP.attlocal.net>
2022-12-14 23:17:05 -0800

    Build navbar

--- pages/navbar.js
+++ pages/navbar.js
@@ -1,13 +1,34 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
-    <header>
-      <nav>
-        <ul className="flex gap-4">
-          <li>Home</li>
-          <li>TL;DR</li>
-          <li>QnA</li>
-        </ul>
-      </nav>
-    </header>
+    <nav className="flex w-1/2">
+      <ul className="flex justify-end gap-8 sm:gap-8 w-full">
+        <li>
+          <Link
+            className="decoration-slate-500 hover:underline hover:underline-offset-8"
+            href="/"
+          >
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link
+            className="decoration-slate-500 hover:underline hover:underline-offset-8"
+            href="/"
+          >
+            TL;DR
+          </Link>
+        </li>
+        <li>
+          <Link
+            className="decoration-slate-500 hover:underline hover:underline-offset-8"
+            href="/"
+          >
+            QnA
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
