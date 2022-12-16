27b58dadeef1897131174999a92ead19c50ee09f
Erin Park <b0ldrobott@Erins-MBP.attlocal.net>
2022-12-15 22:57:54 -0800

    Fix hr tag

--- components/aiResponse.js
+++ components/aiResponse.js
@@ -1,11 +1,13 @@
-export default function AiResponse({ path, summary }) {
-  const responseText = path === "q&a" ? "Answer: " : "Summary: ";
+export default function AiResponse({ apiResponse, responseText }) {
   return (
-    <div className="flex flex-col justify-between w-1/2 ">
-      <hr className="my-5 bg-gray-200 border-0 h-px dark:bg-gray-700" />
-      <div className="p-1 rounded-md ">
-        {responseText} {summary ? <p className="w-100 p-1">{summary}</p> : null}
-      </div>
+    <div className="flex flex-col justify-between min-w-full">
+      <hr className="my-6 bg-gray-200 border-1 h-px dark:bg-gray-700" />
+      {apiResponse ? (
+        <div className="p-1 rounded-md">
+          <div className="text-gray-500">{apiResponse && responseText} </div>
+          {apiResponse ? <p className="p-1">{apiResponse}</p> : null}
+        </div>
+      ) : null}
     </div>
   );
 }
