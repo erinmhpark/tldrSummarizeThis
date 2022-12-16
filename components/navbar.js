67400796ff58b0d964572f919e67e823bcc2904c
Erin Park <b0ldrobott@Erins-MBP.attlocal.net>
2022-12-15 19:00:59 -0800

    Edit Navbar to have two Link components

--- components/navbar.js
+++ components/navbar.js
@@ -1,31 +1,36 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import React from "react";
 
 export default function Navbar() {
+  const router = useRouter();
+  const pathname = router.pathname;
+
   return (
-    <nav className="flex w-1/2 sm:pb-20 sm:-mt-8">
-      <ul className="flex justify-end gap-8 sm:gap-8 w-full">
+    <nav className="font-semibold leading-tight mt-8">
+      <ul className="flex gap-8 items-center justify-center">
         <li>
           <Link
-            className="decoration-slate-500 hover:underline hover:underline-offset-8"
             href="/"
+            className={
+              pathname === "/"
+                ? "underline underline-offset-4 decoration-white"
+                : ""
+            }
           >
-            Home
+            Tl;DR
           </Link>
         </li>
         <li>
           <Link
-            className="decoration-slate-500 hover:underline hover:underline-offset-8"
-            href="/"
+            href="/q&a"
+            className={
+              pathname === "/q&a"
+                ? "underline underline-offset-4 decoration-white"
+                : ""
+            }
           >
-            TL;DR
-          </Link>
-        </li>
-        <li>
-          <Link
-            className="decoration-slate-500 hover:underline hover:underline-offset-8"
-            href="/qna"
-          >
-            QnA
+            Q&A
           </Link>
         </li>
       </ul>
